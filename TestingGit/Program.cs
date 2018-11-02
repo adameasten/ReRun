01bada6d9c52b320f9962fd7359c3c6da01106bf
Adam <adaost0110@hotmail.com>
2018-11-02 19:05:12 +0100

    Added a test message!

--- TestingGit/Program.cs
+++ TestingGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestingGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing git!");
         }
     }
 }
